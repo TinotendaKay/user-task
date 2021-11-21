c58f8ac84a99d066279456525a342f6b3f20c28c
Tinotenda Kanyandura <tinotenda.kanyandura@standardbank.co.za>
2021-11-22 00:47:36 +0200

    [Tino] First commit

---
+++ src/test/java/com/tinotenda/usertask/UserTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinotenda.usertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
